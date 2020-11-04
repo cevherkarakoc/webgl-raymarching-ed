fae96c1ffd8b4ad4be8d41549badb14e89c2d2b4
cvkarakoc.vbt <cevherveli.karakoc@vbt.com.tr>
2020-11-04 11:46:30 +0300

    Add share button

--- src/LiveEditor.js
+++ src/LiveEditor.js
@@ -1,7 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {useState, useCallback } from 'react';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-glsl';
 
 import { Viewer } from './viewer';
@@ -17,10 +18,29 @@ float getSceneDist(vec3 p) {
 `;
 
 export const LiveEditor = ({ initialCode, height = 500, customScene = false }) => {
+  const [copied, setCopied] = useState(false);
   const [code, setCode] = useState(initialCode);
+  const share = useCallback(
+    () => {
+      const baseUrl = `${window.location.origin}${window.location.pathname}`;
+      const shareUrl = `${baseUrl}?code=${btoa(code)}&type=${customScene ? 'customScene' : 'sdf'}`;
+
+      const el = document.createElement('textarea');
+      el.value = shareUrl;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 1000)
+    },
+    [customScene, code],
+  );
 
   return (
-    <div style={{ background: "#2d2d2d", padding: 8, display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+    <div style={{ background: "#2d2d2d", padding: 8, position: "relative", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
       <div style={{ height, width: "100%", overflow: "auto", marginRight: 8, }}>
         <Editor
           value={code}
@@ -34,6 +54,10 @@ export const LiveEditor = ({ initialCode, height = 500, customScene = false }) =
             width: "100%",
           }}
         />
+        <button
+          onClick={() => share()}
+          style={{ position: "absolute", bottom: 0, right: 0, fontSize: 16, paddingTop: 4, paddingBottom: 4, paddingLeft: 12, paddingRight: 12 }}
+        >{copied ? 'Copied' : 'Share'}</button>
       </div>
       <Viewer fragmentSource={ray.replace("%_SDF_%", code + (customScene ? "" : defaultSceneDistFn))} />
     </div>
